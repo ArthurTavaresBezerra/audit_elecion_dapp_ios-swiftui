1a9d728d9148c2e92a4b7e10fbe0d75055b26b28
Arthur Tavares Bezerra <atbezerra@stefanini.com>
2020-02-29 11:32:02 -0300

    wip: configura genesis.json

--- scripts/startNode2.sh
+++ scripts/startNode2.sh
@@ -4,11 +4,11 @@ PORT=30302
 RCPPORT=8542
 IP_LOCAL=$(ipconfig getifaddr en0)
 NETWORK_ID=15
-ENODE=$(bootnode --nodekey private1/geth/nodekey -writeaddress)
+ENODE=$(bootnode --nodekey nodes/private1/geth/nodekey -writeaddress)
 
-echo DATADIR:${DATADIR}, PORT:${PORT}, RCPPORT:${RCPPORT}, IP_LOCAL:${IP_LOCAL}, NETWORK_ID:${NETWORK_ID}, ENODE:${ENODE}
+echo PWD:${PWD}, DATADIR:${DATADIR}, PORT:${PORT}, RCPPORT:${RCPPORT}, IP_LOCAL:${IP_LOCAL}, NETWORK_ID:${NETWORK_ID}, ENODE:${ENODE}
 
-geth --datadir $DATADIR init ./genesis.json 
+geth --datadir $DATADIR init ./nodes/genesis.json 
 geth --datadir $DATADIR account import --password ./accounts/passwdfile  ./accounts/keystore4-ba1e081c82bcddfd2011da6ea97d1e1241959010
 geth console \
 --datadir $DATADIR \
--- scripts/startNode3.sh
+++ scripts/startNode3.sh
@@ -4,11 +4,11 @@ PORT=30303
 RCPPORT=8543
 IP_LOCAL=$(ipconfig getifaddr en0)
 NETWORK_ID=15
-ENODE=$(bootnode --nodekey private1/geth/nodekey -writeaddress)
+ENODE=$(bootnode --nodekey nodes/private1/geth/nodekey -writeaddress)
 
-echo DATADIR:${DATADIR}, PORT:${PORT}, RCPPORT:${RCPPORT}, IP_LOCAL:${IP_LOCAL}, NETWORK_ID:${NETWORK_ID}, ENODE:${ENODE}
+echo PWD:${PWD}, DATADIR:${DATADIR}, PORT:${PORT}, RCPPORT:${RCPPORT}, IP_LOCAL:${IP_LOCAL}, NETWORK_ID:${NETWORK_ID}, ENODE:${ENODE}
 
-geth --datadir $DATADIR init ./genesis.json 
+geth --datadir $DATADIR init ./nodes/genesis.json 
 geth --datadir $DATADIR account import --password ./accounts/passwdfile  ./accounts/keystore5-ea685b9f731aecd7c91a8d98b6f2021e20a5e049
 geth console \
 --datadir $DATADIR \
